b28a36cfe398aaa075b6c43a396bf5a38251fc42
rifqit19 <rifqit19@gmail.com>
2025-01-25 21:02:33 +0700

    create project

---
+++ MovieDB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieDB
+//
+//  Created by rifqi triginandri on 25/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieDBTests/MovieDBTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBTests.swift
+//  MovieDBTests
+//
+//  Created by rifqi triginandri on 25/01/25.
+//
+
+import Testing
+@testable import MovieDB
+
+struct MovieDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
