6257725f9b92939a9e5c8f8a0e8c3689d35be84d
rifqit19 <rifqit19@gmail.com>
2025-01-28 03:46:12 +0700

    Add rowview for movie

---
+++ MovieDB/Views/Cell/MovieRow.swift
@@ -0,0 +1,58 @@
+//
+//  MovieRow.swift
+//  MovieDB
+//
+//  Created by rifqi triginandri on 26/01/25.
+//
+
+
+import SwiftUI
+
+struct MovieRow: View {
+    let movie: Movie
+    
+    var body: some View {
+        HStack {
+            if let posterPath = movie.posterPath, let url = URL(string: APIConfig.imageBaseURL + posterPath) {
+                AsyncImage(url: url) { image in
+                    image.resizable()
+                        .frame(width: 70, height: 100)
+                        .cornerRadius(10)
+                } placeholder: {
+                    ZStack{
+                        Rectangle()
+                            .fill(Color.gray)
+                            .frame(width: 70, height: 100)
+                            .cornerRadius(10)
+                        
+                        ProgressView()
+                    }
+                    
+                }
+            } else {
+                Rectangle()
+                    .fill(Color.gray)
+                .frame(width: 70, height: 100)
+                .cornerRadius(10)
+            }
+            
+            
+            VStack(alignment: .leading) {
+                Text(movie.title)
+                    .font(.headline)
+                
+                Text(movie.releaseDate ?? "Unknown Date")
+                    .font(.caption2)
+                    .foregroundColor(.gray)
+                
+                Text(movie.overview ?? "Unknown Date")
+                    .font(.callout)
+                    .foregroundColor(.gray)
+                    .lineLimit(3)
+
+            }
+            Spacer()
+        }
+        .padding(.vertical, 5)
+    }
+}
