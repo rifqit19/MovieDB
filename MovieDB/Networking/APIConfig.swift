85f0a9ad56e94fb9ab970d27ac0dc5e288997da4
rifqit19 <rifqit19@gmail.com>
2025-01-28 03:47:00 +0700

    Add API config

---
+++ MovieDB/Networking/APIConfig.swift
@@ -0,0 +1,18 @@
+//
+//  APIConfig.swift
+//  FoodFinder
+//
+//  Created by rifqi triginandri on 25/01/25.
+//
+
+
+import Foundation
+
+struct APIConfig {
+    static let baseURL = "https://api.themoviedb.org/3"
+    static let imageBaseURL = "https://image.tmdb.org/t/p/w500"
+    static let trailerBaseURL = "https://www.youtube.com/embed/"
+
+    static let apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjE5NWFhMTcyZWRjMGY3NDFkYzgzZTUyYjQ1YTlmZiIsIm5iZiI6MTcwMTY0MTQzMC44MDcwMDAyLCJzdWIiOiI2NTZjZmNkNjhkZTBhZTAwYzY3Y2Y5ZDUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.kACYXNWns64ekYvJCzyQgAS8COEfDpSf2UzzPwdH1MA"
+
+}
