0a03c12474f71e8d30f0c38887602ee75292add6
rifqit19 <rifqit19@gmail.com>
2025-01-28 03:44:54 +0700

    Add regular movie section

---
+++ MovieDB/Views/Section/RegularSectionView.swift
@@ -0,0 +1,41 @@
+//
+//  SectionView.swift
+//  MovieDB
+//
+//  Created by rifqi triginandri on 26/01/25.
+//
+
+
+import SwiftUI
+
+struct RegularSectionView<Destination: View>: View {
+    let title: String
+    let movies: [Movie]
+    let destination: Destination
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text(title)
+                    .font(.headline)
+                Spacer()
+                NavigationLink(destination: destination) {
+                    Text("See More")
+                        .font(.subheadline)
+                        .foregroundColor(.blue)
+                }
+            }
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 10) {
+                    ForEach(movies, id: \.id) { movie in
+                        NavigationLink(destination: MovieDetailView(movieId: String(movie.id))) {
+                            MovieCard(movie: movie)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                }
+            }
+        }
+        .padding(.horizontal)
+    }
+}
