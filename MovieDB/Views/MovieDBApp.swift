0ebe90fe2de9482e477500f4ac163b47e7ad1cb7
rifqit19 <rifqit19@gmail.com>
2025-01-28 03:43:38 +0700

    Refactor main screen

---
+++ MovieDB/Views/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by rifqi triginandri on 25/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
