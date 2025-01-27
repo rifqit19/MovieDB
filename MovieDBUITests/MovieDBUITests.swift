56415bc75f62bdae64fa4eb669dd86fa4ad15a0d
rifqit19 <rifqit19@gmail.com>
2025-01-28 03:49:26 +0700

    Add UITests

--- MovieDBUITests/MovieDBUITests.swift
+++ MovieDBUITests/MovieDBUITests.swift
@@ -10,31 +10,33 @@ import XCTest
 final class MovieDBUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    override func tearDownWithError() throws {}
 
     @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testEndlessScrolling() throws {
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let movieList = app.scrollViews["movieList"]
+        XCTAssertTrue(movieList.exists, "Movie list should exist.")
+
+        movieList.swipeUp()
+        movieList.swipeUp()
+
+        let allMovies = movieList.images.allElementsBoundByIndex
+        XCTAssertFalse(allMovies.isEmpty, "Movie list should not be empty after scrolling.")
+
+        let lastMovie = allMovies.last
+        XCTAssertNotNil(lastMovie, "There should be a last movie element.")
+        XCTAssertTrue(lastMovie!.exists, "The last movie should be visible.")
     }
 
     @MainActor
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
