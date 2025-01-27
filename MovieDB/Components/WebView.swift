c7e6272c4bc8e35ba5e91c974c984721d866895a
rifqit19 <rifqit19@gmail.com>
2025-01-28 03:48:27 +0700

    Add webview

---
+++ MovieDB/Components/WebView.swift
@@ -0,0 +1,21 @@
+//
+//  WebView.swift
+//  MovieDB
+//
+//  Created by rifqi triginandri on 27/01/25.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(URLRequest(url: url))
+    }
+}
